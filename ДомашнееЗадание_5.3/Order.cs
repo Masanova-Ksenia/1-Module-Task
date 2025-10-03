811cb3afc96ab9b1223f2150bf6fd86e8cc70715
Ксения <masanovaksenya@gmail.com>
2025-10-03 18:27:53 +0500

    Add files via upload

---
+++ ДомашнееЗадание_5.3/Order.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДомашнееЗадание_5._3
+{
+    public interface IDeepCloneable<T>
+    {
+        T Clone();
+    }
+    public class Order : IDeepCloneable<Order>
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Discount> Discounts { get; set; } = new List<Discount>();
+        public decimal DeliveryCost { get; set; }
+        public string PaymentMethod { get; set; }
+        public Order Clone()
+        {
+            var newOrder = new Order
+            {
+                DeliveryCost = this.DeliveryCost,
+                PaymentMethod = this.PaymentMethod
+            };
+            foreach (var product in Products)
+                newOrder.Products.Add(product.Clone());
+
+            foreach (var discount in Discounts)
+                newOrder.Discounts.Add(discount.Clone());
+
+            return newOrder;
+        }
+        public override string ToString()
+        {
+            string productsInfo = string.Join(", ", Products);
+            string discountsInfo = Discounts.Count > 0 ? string.Join("; ", Discounts) : "нет скидок";
+            return $"Заказ:\nТовары: {productsInfo}\nДоставка: {DeliveryCost}\nСкидки: {discountsInfo}\nОплата: {PaymentMethod}";
+        }
+    }
+    public class Product : IDeepCloneable<Product>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Product(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public Product Clone()
+        {
+            return new Product(Name, Price, Quantity);
+        }
+        public override string ToString()
+        {
+            return $"{Name} (Цена: {Price}, Кол-во: {Quantity})";
+        }
+    }
+    public class Discount : IDeepCloneable<Discount>
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+
+        public Discount(string description, decimal amount)
+        {
+            Description = description;
+            Amount = amount;
+        }
+        public Discount Clone()
+        {
+            return new Discount(Description, Amount);
+        }
+        public override string ToString()
+        {
+            return $"{Description}: -{Amount}";
+        }
+    }
+}
---
+++ ДомашнееЗадание_5.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ДомашнееЗадание_5._3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var order1 = new Order
+            {
+                DeliveryCost = 500,
+                PaymentMethod = "Карта"
+            };
+            order1.Products.Add(new Product("Ноутбук", 300000, 1));
+            order1.Products.Add(new Product("Мышь", 5000, 2));
+            order1.Discounts.Add(new Discount("Скидка постоянного клиента", 10000));
+
+            Console.WriteLine("Оригинальный заказ:");
+            Console.WriteLine(order1);
+
+            var order2 = order1.Clone();
+            order2.Products[0].Quantity = 2;
+            order2.PaymentMethod = "Наличные";
+
+            Console.WriteLine("\nКлон заказа:");
+            Console.WriteLine(order2);
+
+            Console.WriteLine("\nПроверка, что оригинал не изменился:");
+            Console.WriteLine(order1);
+        }
+    }
+}
