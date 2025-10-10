7ea01f5c5f85943a53726943e543b515d25b75f6
Ксения <masanovaksenya@gmail.com>
2025-10-10 19:33:07 +0500

    Add files via upload

---
+++ ПрактическаяРабота_5.3/IPrototype.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПрактическаяРабота_5._3
+{
+    public interface IPrototype<T>
+    {
+        T Clone();
+    }
+    public class Weapon : IPrototype<Weapon>
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public Weapon(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+        public Weapon Clone()
+        {
+            return new Weapon(Name, Damage);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (урон: {Damage})";
+        }
+    }
+    public class Armor : IPrototype<Armor>
+    {
+        public string Name { get; set; }
+        public int Defense { get; set; }
+        public Armor(string name, int defense)
+        {
+            Name = name;
+            Defense = defense;
+        }
+        public Armor Clone()
+        {
+            return new Armor(Name, Defense);
+        }
+        public override string ToString()
+        {
+            return $"{Name} (защита: {Defense})";
+        }
+    }
+    public class Skill : IPrototype<Skill>
+    {
+        public string Name { get; set; }
+        public string Type { get; set; } 
+        public int Power { get; set; }
+        public Skill(string name, string type, int power)
+        {
+            Name = name;
+            Type = type;
+            Power = power;
+        }
+        public Skill Clone()
+        {
+            return new Skill(Name, Type, Power);
+        }
+        public override string ToString()
+        {
+            return $"{Name} ({Type}, сила: {Power})";
+        }
+    }
+    public class Character : IPrototype<Character>
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
+
+        public Weapon Weapon { get; set; }
+        public Armor Armor { get; set; }
+        public List<Skill> Skills { get; set; } = new List<Skill>();
+
+        public Character Clone()
+        {
+            Character clone = (Character)this.MemberwiseClone();
+            clone.Weapon = Weapon?.Clone();
+            clone.Armor = Armor?.Clone();
+            clone.Skills = new List<Skill>();
+            foreach (var skill in Skills)
+                clone.Skills.Add(skill.Clone());
+            return clone;
+        }
+        public override string ToString()
+        {
+            string skills = string.Join(", ", Skills);
+            return $"Персонаж: {Name}\n" +
+                   $"Здоровье: {Health}, Сила: {Strength}, Ловкость: {Agility}, Интеллект: {Intelligence}\n" +
+                   $"Оружие: {Weapon}\n" +
+                   $"Броня: {Armor}\n" +
+                   $"Способности: {skills}\n";
+        }
+    }
+}
---
+++ ПрактическаяРабота_5.3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПрактическаяРабота_5._3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Character mage = new Character
+            {
+                Name = "Маг Арктур",
+                Health = 80,
+                Strength = 10,
+                Agility = 15,
+                Intelligence = 30,
+                Weapon = new Weapon("Посох огня", 25),
+                Armor = new Armor("Мантия мудреца", 10),
+                Skills = new List<Skill>
+            {
+                new Skill("Огненный шар", "Магия", 40),
+                new Skill("Щит маны", "Магия", 20)
+            }
+            };
+            Character cloneMage = mage.Clone();
+            cloneMage.Name = "Маг Клон";
+            cloneMage.Weapon.Name = "Посох льда";
+            cloneMage.Skills[0].Name = "Ледяная стрела";
+
+            Console.WriteLine("=== Оригинал ===");
+            Console.WriteLine(mage);
+
+            Console.WriteLine("=== Клон ===");
+            Console.WriteLine(cloneMage);
+        }
+    }
+}
