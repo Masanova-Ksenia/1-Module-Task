033458f7c5128d6bc08ef2401fce3ec0c1ee3dc2
Ксения <masanovaksenya@gmail.com>
2025-10-15 16:31:32 +0500

    Add files via upload

---
+++ HomeWork-6.2/ISubject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6._2
+{
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify();
+    }
+    public class CurrencyExchange : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        private decimal _usdToKztRate;
+
+        public decimal UsdToKztRate
+        {
+            get => _usdToKztRate;
+            set
+            {
+                if (_usdToKztRate != value)
+                {
+                    _usdToKztRate = value;
+                    Console.WriteLine($"\n>>> Курс доллара изменился: 1 USD = {_usdToKztRate} KZT");
+                    Notify();
+                }
+            }
+        }
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+            Console.WriteLine($"{observer.GetType().Name} подписан на обновления курса.");
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+            Console.WriteLine($"{observer.GetType().Name} отписан от обновлений курса.");
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(_usdToKztRate);
+            }
+        }
+    }
+    public class BankObserver : IObserver
+    {
+        public void Update(decimal usdToKztRate)
+        {
+            Console.WriteLine($"Банк: обновился внутренний курс обмена — теперь {usdToKztRate} тенге за доллар.");
+        }
+    }
+    public class InvestorObserver : IObserver
+    {
+        public void Update(decimal usdToKztRate)
+        {
+            if (usdToKztRate > 500)
+                Console.WriteLine($"Инвестор: доллар растёт. Целесообразно купить больше акций в долларах.");
+            else
+                Console.WriteLine($"Инвестор: доллар падает, время продавать");
+        }
+    }
+    public class NewsAgencyObserver : IObserver
+    {
+        public void Update(decimal usdToKztRate)
+        {
+            Console.WriteLine($"Новости: курс доллара изменился и теперь составляет {usdToKztRate} тенге.");
+        }
+    }
+}
---
+++ HomeWork-6.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeWork_6._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            CurrencyExchange exchange = new CurrencyExchange();
+
+            var bank = new BankObserver();
+            var investor = new InvestorObserver();
+            var news = new NewsAgencyObserver();
+
+            exchange.Attach(bank);
+            exchange.Attach(investor);
+            exchange.Attach(news);
+
+            exchange.UsdToKztRate = 480;
+            exchange.UsdToKztRate = 505;
+
+            exchange.Detach(news);
+
+            exchange.UsdToKztRate = 495;
+        }
+    }
+}
