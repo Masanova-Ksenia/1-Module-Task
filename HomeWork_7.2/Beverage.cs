65092e75878ee9ad4b1e6fe6ca14bafe06b1e7b8
Ксения <masanovaksenya@gmail.com>
2025-10-24 01:19:16 +0500

    Add files via upload

---
+++ HomeWork_7.2/Beverage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7._2
+{
+    public abstract class Beverage
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            if (CustomerWantsCondiments())
+                AddCondiments();
+        }
+
+        protected abstract void Brew();
+        protected abstract void AddCondiments();
+
+        private void BoilWater() => Console.WriteLine("Кипятим воду");
+        private void PourInCup() => Console.WriteLine("Наливаем в чашку");
+
+        protected virtual bool CustomerWantsCondiments()
+        {
+            Console.Write("Добавить добавки (y/n)? ");
+            string input = Console.ReadLine()?.Trim().ToLower();
+            while (input != "y" && input != "n")
+            {
+                Console.Write("Некорректный ввод. Введите 'y' или 'n': ");
+                input = Console.ReadLine()?.Trim().ToLower();
+            }
+            return input == "y";
+        }
+    }
+    public class Tea : Beverage
+    {
+        protected override void Brew() => Console.WriteLine("Завариваем чай");
+        protected override void AddCondiments() => Console.WriteLine("Добавляем лимон");
+    }
+    public class Coffee : Beverage
+    {
+        protected override void Brew() => Console.WriteLine("Завариваем кофе");
+        protected override void AddCondiments() => Console.WriteLine("Добавляем молоко и сахар");
+    }
+    public class HotChocolate : Beverage
+    {
+        protected override void Brew() => Console.WriteLine("Растапливаем шоколад и смешиваем с молоком");
+        protected override void AddCondiments() => Console.WriteLine("Добавляем маршмеллоу");
+    }
+
+}
---
+++ HomeWork_7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7._2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Beverage tea = new Tea();
+            Beverage coffee = new Coffee();
+            Beverage chocolate = new HotChocolate();
+
+            Console.WriteLine("\nПриготовление чая:");
+            tea.PrepareRecipe();
+
+            Console.WriteLine("\nПриготовление кофе:");
+            coffee.PrepareRecipe();
+
+            Console.WriteLine("\nПриготовление горячего шоколада:");
+            chocolate.PrepareRecipe();
+        }
+    }
+}
